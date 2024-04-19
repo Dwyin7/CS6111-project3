925dfee0885237a38bea1805545e5a71b5fad773
Dwyin7 <50932467+davidyinbu@users.noreply.github.com>
2024-04-19 13:26:20 -0400

    Update codebase

--- data_cleaner.py
+++ data_cleaner.py
@@ -8,16 +8,15 @@ import pandas as pd
 # ]
 
 
-
 require_columns = [
-    "ARREST_DATE","OFNS_DESC",
-    "LAW_CAT_CD","ARREST_BORO","AGE_GROUP","PERP_SEX", "PERP_RACE"
+    "ARREST_DATE",
+    "OFNS_DESC",
+    "LAW_CAT_CD",
+    "ARREST_BORO",
+    "AGE_GROUP",
+    "PERP_SEX",
+    "PERP_RACE",
 ]
-
-
-
-
-
 
 
 def main():
@@ -28,10 +27,11 @@ def main():
     df = df[df[require_columns].notnull().all(1)]
     # df['KY_CD'] = df['KY_CD'].apply(lambda x:str(int(x)))
     # df['ARREST_PRECINCT'] = df['ARREST_PRECINCT'].apply(lambda x:str(int(x)))
-    df['ARREST_DATE'] = df['ARREST_DATE'].apply(lambda x: x[:2])
+    df["ARREST_DATE"] = df["ARREST_DATE"].apply(lambda x: x[:2])
     df = pd.get_dummies(df)
     print(df.columns)
-    df.to_csv('INTEGRATED-DATASET.csv', index=False)  
+    df.to_csv("INTEGRATED-DATASET.csv", index=False)
+
 
 if __name__ == "__main__":
     main()
--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import pandas as pd
 import sys
 import collections
 from apriori_gen import apriori_gen_with_prune, calculate_conf
+
+
 def init_l1(supp, data):
-    #return list of (itemset, supp)
+    # return list of (itemset, supp)
     length = len(data)
     supports = data.sum() / length
     filtered_col_names = supports[supports >= supp].index.tolist()
@@ -13,33 +15,40 @@ def init_l1(supp, data):
 
 def remove_supp(li):
     return [item[0] for item in li]
-    
+
 
 def apriori(l1, baseket, supp):
-    #l1: [[id],...]
-    
+    # l1: [[id],...]
+
     # res stores [l1,l2,...]
     res = [l1]
     while len(res[-1]) > 0:
         raw_li = remove_supp(res[-1])
-        print(raw_li)
         ck = apriori_gen_with_prune(raw_li)
-        print("ck length: ",len(ck))
+        # print("ck length: ", len(ck))
         mp = collections.defaultdict(int)
         for t in baseket:
-            ct = subset(ck,t)
+            ct = subset(ck, t)
             for c in ct:
                 mp[tuple(c)] += 1
         items = mp.items()
-        lk = [[list(k),v/len(baseket)] for k,v in items if v >= len(baseket)*supp] #tuple list
-        print("lk length: ",len(lk))
+        lk = [
+            [list(k), v / len(baseket)] for k, v in items if v >= len(baseket) * supp
+        ]  # tuple list
+        # print("lk length: ", len(lk))
         res.append(lk)
-        print("result length :", len(res))
+        # print("result length :", len(res))
     return res
 
-def subset(ck, t:list):
+
+def print_list(lst):
+    s = ",".join(lst)
+    return f"[{s}]"
+
+
+def subset(ck, t: list):
     # t is the transcations
-    #set of set
+    # set of set
     t = set(t)
     res = []
     for tp in ck:
@@ -48,29 +57,29 @@ def subset(ck, t:list):
             res.append(tp)
     return res
 
+
 def get_basket(data):
     temp = data.values.tolist()
     basket = []
     for idxs in temp:
-        basket.append([idx for idx,t in enumerate(idxs) if t])
+        basket.append([idx for idx, t in enumerate(idxs) if t])
     return basket
+
 
 def test():
     data = [
-        ['pen', 'ink', 'diary', 'soap'],
-        ['pen', 'ink', 'diary'],
-        ['pen', 'diary'],
-        ['pen', 'ink', 'soap']
+        ["pen", "ink", "diary", "soap"],
+        ["pen", "ink", "diary"],
+        ["pen", "diary"],
+        ["pen", "ink", "soap"],
     ]
     df = pd.DataFrame(data)
+
     def expand_row(row):
         return pd.Series({item: True for item in row if pd.notna(item)})
+
     one_hot_encoded_df = df.apply(expand_row, axis=1).fillna(False)
-    print(one_hot_encoded_df)
-    
     return one_hot_encoded_df
-
-
 
 
 def main():
@@ -80,26 +89,25 @@ def main():
     dataset_file = sys.argv[1]
     supp = float(sys.argv[2])
     conf = float(sys.argv[3])
-    
-    
-    dataset_file = 'INTEGRATED-DATASET.csv'
+
+    dataset_file = "INTEGRATED-DATASET.csv"
 
     df = pd.read_csv(dataset_file)
-    
-    df = test()
-    
+
+    # df = test()
+
     headers = list(df.columns)
-    print("header length",len(headers), headers)
-    
-    
-    #change col name 
-    new_column_names = {old_name: int(index) for index, old_name in enumerate(df.columns)}
+    # print("header length", len(headers), headers)
+
+    # change col name
+    new_column_names = {
+        old_name: int(index) for index, old_name in enumerate(df.columns)
+    }
     df.rename(columns=new_column_names, inplace=True)
-    l1 = init_l1(supp,df)
-    
+    l1 = init_l1(supp, df)
+
     baseket = get_basket(df)
 
-    
     res_idxs = apriori(l1, baseket, supp)
     res = []
     for li in res_idxs:
@@ -108,20 +116,31 @@ def main():
             temp2 = []
             for i in lj[0]:
                 temp2.append(headers[i])
-            temp.append(temp2)
+            temp.append([temp2, lj[1]])
+        if not temp:
+            break
         res.append(temp)
-    print(res)
 
     print("start testing.....")
+    print(f"==Frequent itemsets (min_sup={supp*100}%")
+    conf_rules = []
+    for li in res:
+        for its in li:
+            conf_rules.append(its)
+    ordered_conf_rules = sorted(conf_rules, key=lambda x: x[1], reverse=True)
+    for rule in ordered_conf_rules:
+        print(f"{print_list(rule[0])}, {rule[1]*100}%")
 
-    rules =calculate_conf(res_idxs, conf)
+    rules = calculate_conf(res_idxs, conf)
 
-    for key in rules.keys():
-        print(f'{[headers[i] for i in key[:-1]]} => {headers[key[-1]]} : {rules[key]}')
-    print(len(rules))
-            
-    
+    ordered_rules = list(rules.items())
+    ordered_rules = sorted(ordered_rules, key=lambda x: x[1], reverse=True)
 
+    print(f" \n\n==High-confidence association rules (min_conf={conf*100}%)")
+    for key in ordered_rules:
+        print(
+            f"{print_list([headers[i] for i in key[0][:-1]])} => [{headers[key[0][-1]]}]  (Conf:{key[1][0]*100}%, Supp:{key[1][1]*100}%)"
+        )
 
 
 if __name__ == "__main__":
